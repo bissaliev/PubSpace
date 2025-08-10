3d5325cb7596cdc3c5758b0ed1b2e55ec22c154e
bissaliev <bissaliev21@gmail.com>
2025-08-10 08:56:44 +0400

    Add AuthService in src/api/auth.py

--- src/api/auth.py
+++ src/api/auth.py
@@ -3,17 +3,15 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
-from src.api.dependencies import JWTAuthBackendDeps
+from src.api.dependencies import AuthServiceDeps
 from src.schemas.users import Token
 
 router = APIRouter()
 
 
-@router.post(
-    "/login", response_model=Token, summary="Получение токена аутентификации"
-)
+@router.post("/login", response_model=Token, summary="Получение токена аутентификации")
 async def login(
-    auth_backend: JWTAuthBackendDeps,
+    service: AuthServiceDeps,
     credentials: Annotated[OAuth2PasswordRequestForm, Depends()],
-):
-    return await auth_backend.login(credentials.username, credentials.password)
+) -> Token:
+    return await service.login(credentials.username, credentials.password)
