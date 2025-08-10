206a09da10eb0c36ce62c3a42115804eda508a2a
bissaliev <bissaliev21@gmail.com>
2025-08-10 08:54:07 +0400

    Add services in dependencies

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,41 +1,46 @@
 import uuid
+from collections.abc import AsyncGenerator
 from typing import Annotated
 
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.auth.authentication_backend import JWTAuthenticationBackend
-from src.auth.exceptions import UserNotExists
 from src.db.database import get_db
-from src.managers.user_manager import UserManager
+from src.exceptions.users import InvalidVerifyToken, UserNotExists
 from src.models.users import User
+from src.repositories.user_repo import UserRepository
+from src.services.auth_service import AuthService
+from src.services.user_service import UserService
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 TokenDeps = Annotated[OAuth2PasswordBearer, Depends(oauth2_scheme)]
+
+
 SessionDeps = Annotated[AsyncSession, Depends(get_db)]
 
 
-async def get_user_manager(session: SessionDeps):
-    yield UserManager(session)
+async def get_user_service(session: SessionDeps) -> AsyncGenerator[UserService, None]:
+    yield UserService(session, UserRepository(session))
 
 
-UserManagerDeps = Annotated[UserManager, Depends(get_user_manager)]
+async def get_auth_service(session: SessionDeps) -> AsyncGenerator[AuthService, None]:
+    yield AuthService(session, UserRepository(session))
 
 
-def get_auth_backend(user_manager: UserManagerDeps):
-    return JWTAuthenticationBackend(user_manager)
+UserServiceDeps = Annotated[UserService, Depends(get_user_service)]
+AuthServiceDeps = Annotated[AuthService, Depends(get_auth_service)]
 
 
-JWTAuthBackendDeps = Annotated[
-    JWTAuthenticationBackend, Depends(get_auth_backend)
-]
-
-
-async def get_current_user(
-    token: TokenDeps, auth_backend: JWTAuthBackendDeps
-) -> User:
-    return await auth_backend.get_current_user(token)
+async def get_current_user(token: TokenDeps, service: AuthServiceDeps) -> User:
+    try:
+        return await service.get_current_user(token)
+    except (UserNotExists, InvalidVerifyToken):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        ) from None
 
 
 CurrentUser = Annotated[User, Depends(get_current_user)]
@@ -47,31 +52,25 @@ async def get_superuser(user: CurrentUser) -> User:
     return user
 
 
-async def get_active_user(user: Annotated[User, Depends(get_current_user)]):
+async def get_active_user(user: Annotated[User, Depends(get_current_user)]) -> User:
     if not user.is_active:
-        from fastapi import HTTPException, status
-
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user"
         )
     return user
 
 
-async def get_verified_user(user: Annotated[User, Depends(get_active_user)]):
+async def get_verified_user(user: Annotated[User, Depends(get_active_user)]) -> User:
     if not user.is_verified:
-        from fastapi import HTTPException, status
-
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="Unverified user"
         )
     return user
 
 
-async def get_user_or_404(
-    id: uuid.UUID, user_manager: UserManagerDeps
-) -> User:
+async def get_user_or_404(id: uuid.UUID, user_service: UserServiceDeps) -> User:
     try:
-        user = await user_manager.get(id)
+        user = await user_service.repo.get_by_id(id)
         return user
     except UserNotExists as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND) from e
