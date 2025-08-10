2658da8fbdeba5e50f52da2c44390e81b07cbf81
bissaliev <bissaliev21@gmail.com>
2025-08-10 08:57:13 +0400

    Add UserService in src/api/users.py

--- src/api/users.py
+++ src/api/users.py
@@ -2,11 +2,11 @@ from fastapi import APIRouter, Depends, HTTPException, status
 
 from src.api.dependencies import (
     CurrentUser,
-    UserDeps,
-    UserManagerDeps,
+    UserServiceDeps,
     get_superuser,
 )
-from src.auth.exceptions import InvalidPassword, UserAlreadyExists
+from src.dtos.users import UserUpdateDTO
+from src.exceptions.users import UserAlreadyExists, UserNotExists
 from src.schemas.users import UserCreate, UserRead, UserUpdate
 
 router = APIRouter()
@@ -18,10 +18,9 @@ router = APIRouter()
     dependencies=[Depends(get_superuser)],
     summary="Список пользователей",
 )
-async def get_users(
-    user_manager: UserManagerDeps, limit: int = 10, offset: int = 0
-):
-    return await user_manager.list(limit, offset)
+async def get_users(user_service: UserServiceDeps) -> list[UserRead]:
+    users = await user_service.get_users()
+    return users
 
 
 @router.post(
@@ -30,32 +29,28 @@ async def get_users(
     status_code=status.HTTP_201_CREATED,
     summary="Регистрация пользователя",
 )
-async def register(user_create: UserCreate, user_manager: UserManagerDeps):
+async def register(user_create: UserCreate, user_service: UserServiceDeps) -> UserRead:
     try:
-        created_user = await user_manager.create(user_create)
+        created_user = await user_service.create_user(user_create)
     except UserAlreadyExists as e:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="REGISTER_USER_ALREADY_EXISTS",
         ) from e
-    except InvalidPassword as e:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="REGISTER_INVALID_PASSWORD",
-        ) from e
     return created_user
 
 
 @router.get("/me", response_model=UserRead, summary="Профиль пользователя")
-async def me(user: CurrentUser):
+async def me(user: CurrentUser) -> UserRead:
     return user
 
 
 @router.patch("/me", response_model=UserRead, summary="Обновление профиля")
 async def update_me(
-    user: CurrentUser, user_update: UserUpdate, user_manager: UserManagerDeps
-):
-    return await user_manager.update(user, user_update, safe=True)
+    user: CurrentUser, user_update: UserUpdate, user_service: UserServiceDeps
+) -> UserRead:
+    dto = UserUpdateDTO(**user_update.model_dump(exclude_unset=True))
+    return await user_service.update_user(user.id, dto)
 
 
 @router.get(
@@ -64,8 +59,13 @@ async def update_me(
     dependencies=[Depends(get_superuser)],
     summary="Получение пользователя по id",
 )
-async def get_user(user: UserDeps):
-    return user
+async def get_user(id: int, user_service: UserServiceDeps) -> UserRead:
+    try:
+        return await user_service.get_user(id)
+    except UserNotExists:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not exists"
+        ) from None
 
 
 @router.patch(
@@ -75,9 +75,15 @@ async def get_user(user: UserDeps):
     summary="Обновление данных пользователя",
 )
 async def update_user(
-    user: UserDeps, user_update: UserUpdate, user_manager: UserManagerDeps
-):
-    return await user_manager.update(user, user_update, safe=False)
+    id: int, user_update: UserUpdate, user_service: UserServiceDeps
+) -> UserRead:
+    try:
+        dto = UserUpdateDTO(**user_update.model_dump(exclude_unset=True))
+        return await user_service.update_user(id, dto)
+    except UserNotExists:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not exists"
+        ) from None
 
 
 @router.delete(
@@ -86,5 +92,10 @@ async def update_user(
     dependencies=[Depends(get_superuser)],
     summary="Удаление пользователя",
 )
-async def delete_user(user: UserDeps, user_manager: UserManagerDeps):
-    return await user_manager.delete(user)
+async def delete_user(id: int, service: UserServiceDeps) -> None:
+    try:
+        await service.delete_user(id)
+    except UserNotExists:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not exists"
+        ) from None
